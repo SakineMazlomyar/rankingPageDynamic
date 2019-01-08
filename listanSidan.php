943e74b4d10cdd4e0cfa39f218f51ebbe901b1cc
SakineMazlomyar <mazlomyar12@gmail.com>
2019-01-08 14:12:39 +0100

    addedListan

---
+++ listanSidan.php
@@ -0,0 +1,15 @@
+<?php include("./head.php");?>
+<body>
+<?php include("./header.php");?>
+
+
+
+
+<div>
+<?php include("./functionExtention.php");?>
+</div>
+
+
+<?php include("./footer.php");?>
+</body>
+</html>
