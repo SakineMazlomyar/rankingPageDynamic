d3ad45df70072daee1d86ffde7a2ba6c04948191
SakineMazlomyar <mazlomyar12@gmail.com>
2019-01-15 15:01:19 +0100

    recoded

--- descriptionCode.php
+++ descriptionCode.php
@@ -1,16 +1,9 @@
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-    <link rel="stylesheet" href="./style.css">
-</head>
+<?php include("./head.php");?>
 <body>
+<?php include("./header.php");?>
  
-<form method="post" action="./details.php">
+<form method="post" action="./details.php" style="position: relative; top: 15em;">
     <select name="event">
     <option value="html">html</option>
     <option value="css">css</option>
@@ -22,8 +15,9 @@
     <input type="submit" name="submit" value="Get Selected Value's description." />
 </form>
 
+<?php include("./footer.php");?>
 </body>
 </html>
 
-<div>
+
 
