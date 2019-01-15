4080b49c87c3483044e8f2a29334cbaf611018a7
SakineMazlomyar <mazlomyar12@gmail.com>
2019-01-15 12:02:36 +0100

    addedNewFunc

--- functionExtention.php
+++ functionExtention.php
@@ -1,6 +1,6 @@
 <?php
 include("./listan.php");
-
+include("./description.php");
 
 
 sort($myFavLan);
@@ -14,7 +14,29 @@ function sortTheList(){
     };
 }
 
-sortTheList()
+
+function getAllKeys(){
+    foreach($GLOBALS['languagesDescription'] as $key=>$value){
+
+        echo
+        "<li id='dropDown'>"."<a href='details.php'>". $key ."</a>"."</li>";
+    }
+}
+
+function getValues(){
+    $array = array();
+    foreach($GLOBALS['languagesDescription'] as $key=>$value){
+        array_push($array, $key);
+
+    }
+
+   /*  foreach($array as $value){
+        if($value == $GLOBALS['languagesDescription']=>[$key])
+        echo $GLOBALS['languagesDescription'][$value];
+        return;
+    } */
+}
+
 
 
 ?>
