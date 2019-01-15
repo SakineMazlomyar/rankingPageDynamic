a7c5c0bb7236586d47d46719cdfeeea77994b9f0
SakineMazlomyar <mazlomyar12@gmail.com>
2019-01-15 12:03:24 +0100

    addedAssociativeArray

---
+++ description.php
@@ -0,0 +1,16 @@
+
+<?php
+
+$languagesDescription = array(
+
+    "html"=>"Hypertext text makrup language. This is a frontend language.",
+    "css"=>"Cascading style sheet, This is a design language in frontend. ",
+    "javascript"=>"Javascript is a frontend and even uses as backend with nodejs.",
+    "nodejs"=>"Nodejs is not a laguage. It is a framwork for package and even mangage backend with js.",
+    "php"=>"Hypertext proprcesser is a backend language which uses with a database.",
+    "python"=>"Python is a backend language which can uses in backend games and osv.",
+);
+
+
+
+?>
