8c9f84198d5fb6981a9c919e83a1004f22217a35
SakineMazlomyar <mazlomyar12@gmail.com>
2019-01-28 15:06:44 +0100

    modifiedValues

--- listan.php
+++ listan.php
@@ -1,13 +1,13 @@
 <?php 
-
 $myFavLan= array(
     
     "2.html",
-    "1.css",
-    "3.javascript",
-    "4.nodeJs",
-    "5.php",
-    "6.python",
+    "0.css",
+    "44.javascript",
+    "40.nodeJs",
+    "100.php",
+    "200.python",
+
 );
 
 
